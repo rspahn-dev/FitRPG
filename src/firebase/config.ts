ec9e64f5f8a312bd5d7adc26d136085a91a45727
Robin Spahn <rspahn@alumni.albany.edu>
2025-10-27 20:05:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6039604360-99b23",
+  "appId": "1:1013549784863:web:e3540895a472c6a0409a2a",
+  "apiKey": "AIzaSyDiMYCGA6iM8PqgVkXdpFNue1SeBO_8zCU",
+  "authDomain": "studio-6039604360-99b23.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1013549784863"
+};
