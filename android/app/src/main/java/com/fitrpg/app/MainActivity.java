8d3b014d3675445af782b3e09ebfb3de008baed2
Robin Spahn <rspahn@alumni.albany.edu>
2025-10-28 21:34:40 +0000

    now what

---
+++ android/app/src/main/java/com/fitrpg/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fitrpg.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fitrpg.app',
+  appName: 'FitRPG',
+  webDir: 'out'
+};
+
+export default config;
